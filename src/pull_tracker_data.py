b43cff214f908b74b7698785789ba21ce5868735
Victor Faner <vfaner@uplifteducation.org>
2019-08-20 16:55:20 -0500

    Handle Summit PS pulls Limit table to non-empty rows before loading

--- src/pull_tracker_data.py
+++ src/pull_tracker_data.py
@@ -28,26 +28,26 @@ def main():
     campuses = ['North Hills PS', 'Peak PS', 'Ascend PS', 'Elevate PS',
                 'Gradus PS', 'Grand PS', 'Hampton PS', 'Heights PS',
                 'Infinity PS', 'Luna PS', 'Meridian PS', 'Mighty PS',
-                'Pinnacle PS', 'Summit PS', 'Triumph PS', 'White Rock Hills PS',
-                'Williams PS', 'Wisdom PS', 'Uplift Lee PS']
+                'Pinnacle PS', 'Triumph PS', 'White Rock Hills PS',
+                'Williams PS', 'Wisdom PS', 'Uplift Lee PS', 'Summit PS']
     dtypes = {
-		'Status'		  : types.VARCHAR(),
-        'August Formal'   : types.VARCHAR(3),
-        '9-Sep'           : types.VARCHAR(3),
-        '23-Sep'          : types.VARCHAR(3),
-        '7-Oct'           : types.VARCHAR(3),
-        '21-Oct'          : types.VARCHAR(3),
-        '4-Nov'           : types.VARCHAR(3),
-        '18-Nov'          : types.VARCHAR(3),
-        'December Formal' : types.VARCHAR(3),
-        '3-Feb'           : types.VARCHAR(3),
-        '17-Feb'          : types.VARCHAR(3),
-        '2-Mar'           : types.VARCHAR(3),
-        '16-Mar'          : types.VARCHAR(3),
-        '30-Mar'          : types.VARCHAR(3),
-        '13-Apr'          : types.VARCHAR(3),
-        '27-Apr'          : types.VARCHAR(3),
-        'May Formal'      : types.VARCHAR(3),
+		'Status'		 : types.VARCHAR(),
+        'August Formal'  : types.VARCHAR(3),
+        '9-Sep'          : types.VARCHAR(3),
+        '23-Sep'         : types.VARCHAR(3),
+        '7-Oct'          : types.VARCHAR(3),
+        '21-Oct'         : types.VARCHAR(3),
+        '4-Nov'          : types.VARCHAR(3),
+        '18-Nov'         : types.VARCHAR(3),
+        'December Formal': types.VARCHAR(3),
+        '3-Feb'          : types.VARCHAR(3),
+        '17-Feb'         : types.VARCHAR(3),
+        '2-Mar'          : types.VARCHAR(3),
+        '16-Mar'         : types.VARCHAR(3),
+        '30-Mar'         : types.VARCHAR(3),
+        '13-Apr'         : types.VARCHAR(3),
+        '27-Apr'         : types.VARCHAR(3),
+        'May Formal'     : types.VARCHAR(3),
     }
 
     dfs = []
@@ -61,12 +61,73 @@ def main():
                                            include_tailing_empty=True)
                 df['Campus'] = campus
                 df['Grade Level'] = grade_sheet.title
+
+                if campus == 'Summit PS':  # Handle Summit format change
+                    df['Scholar Name'] = df['Scholar Name'] + ', ' + df['Status']
+                    df['StudentID'] = None
+                    df['Status'] = None
+                    df['Employee ID'] = None
+
+                df = df[
+                    ['Employee ID',
+                     'Teacher Name',
+                     'StudentID',
+                     'Scholar Name',
+                     'Status',
+                     'August Formal',
+                     '9-Sep',
+                     '23-Sep',
+                     '7-Oct',
+                     '21-Oct',
+                     '4-Nov',
+                     '18-Nov',
+                     'December Formal',
+                     '3-Feb',
+                     '17-Feb',
+                     '2-Mar',
+                     '16-Mar',
+                     '30-Mar',
+                     '13-Apr',
+                     '27-Apr',
+                     'May Formal',
+                     'Campus',
+                     'Grade Level']
+                ]
+
                 dfs.append(df)
 
     master_df = (
         pd.concat(dfs, sort=False).astype(str)
-            .replace('', np.nan)
-            .replace('nan', np.nan)
+            .replace(r'', np.nan)
+            .replace(r'nan', np.nan)
+            .replace(r'None', np.nan)
+            .replace(r'#N/A', np.nan)
+            .replace(r'#REF!', np.nan)
+            .replace(r'#REF!,', np.nan)
+            .replace(r'#REF!, ', np.nan)
+            .replace(r', ', np.nan)
+            .dropna(how='all',
+                    subset=['Employee ID',
+                            'Teacher Name',
+                            'StudentID',
+                            'Scholar Name',
+                            'Status',
+                            'August Formal',
+                            '9-Sep',
+                            '23-Sep',
+                            '7-Oct',
+                            '21-Oct',
+                            '4-Nov',
+                            '18-Nov',
+                            'December Formal',
+                            '3-Feb',
+                            '17-Feb',
+                            '2-Mar',
+                            '16-Mar',
+                            '30-Mar',
+                            '13-Apr',
+                            '27-Apr',
+                            'May Formal'])  # Remove rows with no data
     )
     
     master_df.to_sql('bas_fp_tracker_data_19_20', con=engine, schema='DAT',
