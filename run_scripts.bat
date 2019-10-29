6dc527bc3242ec3455bfb4d079b5b0b61d781d17
Victor Faner <vfaner@uplifteducation.org>
2019-10-29 12:26:24 -0500

    Change venv to system Python
    Add additional Tableau file types to .gitignore

--- run_scripts.bat
+++ run_scripts.bat
@@ -1,5 +1,4 @@
 :: Update SQL master roster
-call venv\Scripts\activate.bat
 sqlcmd -S TLXSQLPROD-01 -E -i queries\update_bas_fp_rosters.sql 
 
 :: Update tracker rosters and pull SQL master tracker data
