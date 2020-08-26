b39b0db51e3df4715f57450c0e0fa7a228362b0c
Victor Faner <vfaner@uplifteducation.org>
2020-08-26 14:16:36 -0500

    Add roster table for 20-21

--- queries/create_initial_roster_table.sql
+++ queries/create_initial_roster_table.sql
@@ -4,24 +4,53 @@
     description: Create initial roster table for BAS/F&P Trackers
 */
 
+-- Store vRoster as a temp table for optimization purposes
+DROP TABLE IF EXISTS #vRoster
+SELECT * INTO #vRoster FROM ODS_CPS.RPT.vRoster
+
+-- Create Roster Table
 DROP TABLE IF EXISTS ODS_CPS.DAT.bas_fp_roster_20_21
+CREATE TABLE ODS_CPS.DAT.bas_fp_roster_20_21(
+    StudentID               FLOAT
+    ,StudentName            VARCHAR(MAX)
+    ,GradeLevel             INT
+    ,SchoolNameAbbreviated  VARCHAR(MAX)
+    ,initial_roster         INT  -- indicator marking initial rosters, used in case
+                                 -- a tracker needs to be rebuilt/debugged later on
+    ,row_id                 INT
+)
+
+INSERT INTO ODS_CPS.DAT.bas_fp_roster_20_21
 SELECT DISTINCT
-    TeacherNumber
-    ,TeacherName
-    ,StudentID
+    StudentID
     ,StudentName
     ,GradeLevel
-    ,SchoolNameAbbreviated
-    ,TeacherEmail
 
-INTO ODS_CPS.DAT.bas_fp_roster_20_21
+    -- Normalize Lee/Delmas
+    ,CASE
+        WHEN SchoolNameAbbreviated LIKE '%Lee%' THEN 'Uplift Delmas Morton PS'
+        WHEN SchoolNameAbbreviated LIKE '%Delmas%' THEN 'Uplift Delmas Morton PS'
+        ELSE SchoolNameAbbreviated
+    END AS SchoolNameAbbreviated
+    ,1 AS initial_roster
 
-FROM ODS_CPS.RPT.vRosterCurrent
+    ,ROW_NUMBER() OVER(
+        ORDER BY
+            StudentID
+            ,StudentName
+            ,GradeLevel
+            ,CASE
+                WHEN SchoolNameAbbreviated LIKE '%Lee%' THEN 'Uplift Delmas Morton PS'
+                WHEN SchoolNameAbbreviated LIKE '%Delmas%' THEN 'Uplift Delmas Morton PS'
+                ELSE SchoolNameAbbreviated
+            END
+    ) AS row_id
 
-WHERE GradeLevel BETWEEN 0 AND 5
+FROM #vRoster
 
-ORDER BY 
-    SchoolNameAbbreviated
-    ,GradeLevel
-    ,TeacherName
-    ,StudentName
+WHERE
+    GETDATE() BETWEEN StudentEnrollDate AND StudentExitDate
+    AND GETDATE() BETWEEN SchoolEntryDate AND SchoolExitDate
+    AND GETDATE() BETWEEN SectionStartDate AND SectionEndDate
+    AND GradeLevel BETWEEN 0 AND 5
+
