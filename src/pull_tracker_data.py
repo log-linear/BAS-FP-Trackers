0d45c6bb8bf60f4ae4836a85cbf58bb5bbd8f3f6
Victor Faner <vfaner@uplifteducation.org>
2020-09-01 13:59:12 -0500

    20-21 update

--- src/pull_tracker_data.py
+++ src/pull_tracker_data.py
@@ -5,8 +5,6 @@ date:          2019-08-12
 description:   Pull data from Trackers into a single table to then be
                loaded into SQL Server.
 """
-from pathlib import Path
-
 import pandas as pd
 import numpy as np
 import pygsheets
@@ -15,10 +13,11 @@ from sqlalchemy import create_engine, types
 
 def main():
     client = pygsheets.authorize(
-        '../client_secret_507650277646-89evt7ufgfmlrfci4043cthvlgi3jf0s.apps.googleusercontent.com.json'
+        '../client_secret_306687575540-dgtvvmcmk3flnvig5mt1j7gk21s5087c.apps.googleusercontent.com.json'
     )
     engine = create_engine(
-        r'mssql+pyodbc://sql-cl-dw-pro\datawarehouse/ODS_CPS?driver=SQL+Server'
+        r'mssql+pyodbc://TLXSQLPROD-01/ODS_CPS?driver=ODBC+Driver+13+for+SQL+Server',
+        fast_executemany=True  # Faster loads - for SQLAlchemy 1.3+ ONLY
     )
     campuses = [
         'North Hills PS',
@@ -61,84 +60,75 @@ def main():
         'May Formal'     : types.VARCHAR(3),
     }
 
+    # Get data from each campus tracker as a list of dataframes
     dfs = []
     for campus in campuses:
         tracker = client.open(f'{campus} 20-21 BAS/F&P Tracker')
 
+        # Loop through each Grade level sheet
         for grade_sheet in tracker.worksheets():
-            if grade_sheet.title not in ['Instructions', 'Data Validation',
-                                         'Roster Validation']:
+            if grade_sheet.title not in ['Instructions', 'Data Validation']:
                 df = grade_sheet.get_as_df(start='A2', end='U2002',
                                            include_tailing_empty=True)
                 df['Campus'] = campus
                 df['Grade Level'] = grade_sheet.title
-
-                if campus == 'Summit PS':  # Handle Summit format change
-                    df['Scholar Name'] = df['Scholar Name'] + ', ' + df['Status']
-                    df['StudentID'] = None
-                    df['Status'] = None
-                    df['Employee ID'] = None
-
                 df = df[
-                    ['Employee ID',
-                     'Teacher Name',
-                     'StudentID',
-                     'Scholar Name',
-                     'Status',
-                     'August Formal',
-                     '9-Sep',
-                     '23-Sep',
-                     '7-Oct',
-                     '21-Oct',
-                     '4-Nov',
-                     '18-Nov',
-                     'December Formal',
-                     '3-Feb',
-                     '17-Feb',
-                     '2-Mar',
-                     '16-Mar',
-                     '30-Mar',
-                     '13-Apr',
-                     '27-Apr',
-                     'May Formal',
-                     'Campus',
-                     'Grade Level']
+                    'Teacher',
+                    'Scholar',
+                    'Status',
+                    'August Formal',
+                    '9-Sep',
+                    '23-Sep',
+                    '7-Oct',
+                    '21-Oct',
+                    '4-Nov',
+                    '18-Nov',
+                    'December Formal',
+                    '3-Feb',
+                    '17-Feb',
+                    '2-Mar',
+                    '16-Mar',
+                    '30-Mar',
+                    '13-Apr',
+                    '27-Apr',
+                    'May Formal'
                 ]
 
                 dfs.append(df)
 
     master_df = (
         pd.concat(dfs, sort=False).astype(str)
-            .replace(r'', np.nan)
-            .replace(r'nan', np.nan)
-            .replace(r'None', np.nan)
-            .replace(r'#N/A', np.nan)
-            .replace(r'#REF!', np.nan)
-            .replace(r'#REF!,', np.nan)
-            .replace(r'#REF!, ', np.nan)
-            .replace(r', ', np.nan)
-            .dropna(how='all',
-                    subset=['Employee ID',
-                            'Teacher Name',
-                            'StudentID',
-                            'Scholar Name',
-                            'Status',
-                            'August Formal',
-                            '9-Sep',
-                            '23-Sep',
-                            '7-Oct',
-                            '21-Oct',
-                            '4-Nov',
-                            '18-Nov',
-                            'December Formal',
-                            '3-Feb',
-                            '17-Feb',
-                            '2-Mar',
-                            '16-Mar',
-                            '30-Mar',
-                            '13-Apr',
-                            '27-Apr',
-                            'May Formal'])  # Remove rows with no data
+            .replace(r'|'  # Handle NaN/None/NULL values
+                     r'nan|'
+                     r'None|'
+                     r'#N/A|'
+                     r'#REF!\W|'
+                     r'#REF!\W +|'
+                     r', +', np.nan)
+            .dropna(  # Remove rows with no data
+                how='all',
+                subset=[
+                    'Teacher',
+                    'Scholar',
+                    'Status',
+                    'August Formal',
+                    '9-Sep',
+                    '23-Sep',
+                    '7-Oct',
+                    '21-Oct',
+                    '4-Nov',
+                    '18-Nov',
+                    'December Formal',
+                    '3-Feb',
+                    '17-Feb',
+                    '2-Mar',
+                    '16-Mar',
+                    '30-Mar',
+                    '13-Apr',
+                    '27-Apr',
+                    'May Formal'
+                ]
+        )
     )
     
     master_df.to_sql('bas_fp_tracker_data_20_21', con=engine, schema='DAT',
